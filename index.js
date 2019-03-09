97a33e10be566dcd1c63cfd847ea505ad9d15784
jlgarciam <joselgarmor@gmail.com>
2019-03-09 13:21:21 +0100

    Añadir mensaje de error

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/init", (req, res) => {
-  res.send("Init Backend");
-});
-
+app
+  .get("/init", (req, res) => {
+    res.send("Init Backend");
+  })
+  .on("error", _ => console.log("Error: Al levantar servidor"));
 app.listen(process.env.PORT || 8080);
-dasdsdasdsfdsfds;
