620159bbfbd357352949f34a28385554a49e11b3
Roberto iMac Pro <r.mantas.nakhai@gmail.com>
2019-03-13 17:26:07 +0100

    Adding proxy with other teams and adding external charts with other APIs

--- public/frontGovernmentAPI/proxyChart.js
+++ public/frontGovernmentAPI/proxyChart.js
@@ -3,17 +3,15 @@ angular
     .controller("governmentsProxyChartCtrl",["$scope","$http",function ($scope, $http){
         
         $scope.apikey = "keyRob";
-        $scope.dataEducation = {};
-        $scope.dataWages = {};
+        $scope.dataco2 = {};
+        $scope.dataGovern = {};
         var dataCacheEducation = {};
         var dataCacheWages = {};
         $scope.categorias = [];
         $scope.categorias1 = [];
-        //G08
-        $scope.year = [];
-        $scope.varied = [];
-        $scope.averageWage =[];
-        //G07
+        //team03
+        $scope.datoExterno = [];
+        //Me
         $scope.year1 = [];
         $scope.trustGovernment = [];
         $scope.generosity = [];
@@ -26,13 +24,10 @@ angular
      $http.get("/proxy/governments").then(function(response){
                 
                 dataCacheEducation = response.data;
-                $scope.dataEducation =dataCacheEducation;
+                $scope.dataco2 =dataCacheEducation;
                 
                 for(var i=0; i<response.data.length; i++){
-                    $scope.categorias.push($scope.dataEducation[i].province);
-                    $scope.year.push(Number($scope.dataEducation[i].year));
-                    $scope.varied.push(Number($scope.dataEducation[i].varied));
-                    $scope.averageWage.push(Number($scope.dataEducation[i].averageWage));
+                    $scope.datoExterno.push(Number($scope.dataco2[i]["co2-metrics-tons-per-capita"]));
                 }
                 
                 
@@ -41,20 +36,20 @@ angular
             $http.get("/api/v1/governments"+ "?" + "apikey=" + $scope.apikey).then(function(response){
                 
                 dataCacheWages = response.data;
-                $scope.dataWages =dataCacheWages;
+                $scope.dataGovern =dataCacheWages;
                 
                 for(var i=0; i<response.data.length; i++){
-                    $scope.categorias1.push($scope.dataWages[i].country);
-                    $scope.year1.push(Number($scope.dataWages[i]["year"]));
-                    $scope.trustGovernment.push(Number($scope.dataWages[i]["trustGovernment"]));
-                    $scope.generosity.push(Number($scope.dataWages[i]["generosity"]));
-                    $scope.confidence.push(Number($scope.dataWages[i]["confidence"]));
+                    $scope.categorias1.push($scope.dataGovern[i].country);
+                    $scope.year1.push(Number($scope.dataGovern[i]["year"]));
+                    $scope.trustGovernment.push(Number($scope.dataGovern[i]["trustGovernment"]));
+                    $scope.generosity.push(Number($scope.dataGovern[i]["generosity"]));
+                    $scope.confidence.push(Number($scope.dataGovern[i]["confidence"]));
                 }
 
 
                     Highcharts.chart('container',{
                         title: {
-                            text: 'Integrated Team 03 & G08'
+                            text: 'Integrated Team 03 & Team 5'
                         },
                         chart: {
                             type: 'line'
@@ -84,13 +79,18 @@ angular
                         },
                         series:[{
                             name: 'year',
-                            data: $scope.varied,
+                            data: $scope.datoExterno,
                         },
                         
                         
                         {
                             name: 'Generosity ',
                             data: $scope.generosity
+                        },
+                        
+                        {
+                            name: 'Confidence',
+                            data: $scope.confidence
                         },
                         
                         {
