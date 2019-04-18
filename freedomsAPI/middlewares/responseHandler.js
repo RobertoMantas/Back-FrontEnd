c9ef16ac31cd0a5a0d1b761d29a84583f618a2b4
jlgarciam <joselgarmor@gmail.com>
2019-04-18 19:29:47 +0200

    errorResponse and methodNotAllowed Handlers

--- freedomsAPI/middlewares/responseHandler.js
+++ freedomsAPI/middlewares/responseHandler.js
@@ -1,4 +1,6 @@
 const freedomsCol = require("../models/freedoms");
+const HttpStatus = require('http-status-codes');
+const ClientError = require("../utils/clientError");
 
 const getResponseHandler = async (_req, res, next) => {
     try {
@@ -8,6 +10,17 @@ const getResponseHandler = async (_req, res, next) => {
     }
 };
 
+const errorResponseHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.clientMessage || "Internal Server Error");
+};
+
+const methodNotAllowedHandler = (_req, _res, next) => {
+    next(new ClientError("Method Not Allowed", "Method Not Allowed", HttpStatus.METHOD_NOT_ALLOWED));
+};
+
 module.exports = {
-    getResponseHandler
+    getResponseHandler,
+    methodNotAllowedHandler,
+    errorResponseHandler
 };
